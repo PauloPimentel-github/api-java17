0fc5491792d9ad57c5b374445c75dbc40359c110
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2023-06-29 23:03:43 -0300

    First commit :tada:

---
+++ src/main/java/com/phpimentel/apijava17/ApiJava17Application.java
@@ -0,0 +1,13 @@
+package com.phpimentel.apijava17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJava17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJava17Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/phpimentel/apijava17/ApiJava17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phpimentel.apijava17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJava17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
